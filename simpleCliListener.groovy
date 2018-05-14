682a202313773b0cf7572e8e98bb86f4ff1cf7c3
rnslucas <richard@rnslucas.com>
2018-05-14 08:59:38 +0100

    Initial commit of simple server socker for testing JIRA webhooks

---
+++ simpleCliListener.groovy
@@ -0,0 +1,18 @@
+
+
+def server = new ServerSocket(4040)
+
+while(true) {
+    server.accept{ socket ->
+        println "processing new connection"
+
+        socket.withStreams { input, output ->
+            def json = input.readLines()
+            json.forEach{
+                println it
+            }
+
+        }
+        println "processing/thread complete"
+    }
+}
